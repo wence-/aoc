6f8533e92942078becff7f414317a8b0e982f7f6
Lawrence Mitchell <lawrence@wence.uk>
2020-12-07 22:40:05 +0000

    Option to run everything

--- rust-2020/src/main.rs
+++ rust-2020/src/main.rs
@@ -4,56 +4,57 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        panic!("Need to specify day");
+    let days;
+    if args.len() == 2 {
+        match &args[1].parse::<usize>() {
+            Ok(n) => days = vec![*n],
+            _ => panic!("Couldn't handle argument"),
+        }
+    } else {
+        days = (1..8).collect()
     }
 
-    let day: usize = match &args[1].parse::<usize>() {
-        Ok(n) => *n,
-        Err(_) => {
-            panic!("Could not parse argument '{}' as day", args[1]);
-        }
-    };
-
-    println!("Running day {}", day);
-    match day {
-        1 => {
-            let (a, b) = aoc::day01::run();
-            println!("Part 1: {}", a);
-            println!("Part 2: {}", b);
-        }
-        2 => {
-            let (a, b) = aoc::day02::run();
-            println!("Part 1: {}", a);
-            println!("Part 2: {}", b);
-        }
-        3 => {
-            let (a, b) = aoc::day03::run();
-            println!("Part 1: {}", a);
-            println!("Part 2: {}", b);
-        }
-        4 => {
-            let (a, b) = aoc::day04::run();
-            println!("Part 1: {}", a);
-            println!("Part 2: {}", b);
-        }
-        5 => {
-            let (a, b) = aoc::day05::run();
-            println!("Part 1: {}", a);
-            println!("Part 2: {}", b);
-        }
-        6 => {
-            let (a, b) = aoc::day06::run();
-            println!("Part 1: {}", a);
-            println!("Part 2: {}", b);
-        }
-        7 => {
-            let (a, b) = aoc::day07::run();
-            println!("Part 1: {}", a);
-            println!("Part 2: {}", b);
-        }
-        _ => {
-            panic!("Unhandled day");
-        }
-    };
+    for d in days {
+        println!("Running day {}", d);
+        match d {
+            1 => {
+                let (a, b) = aoc::day01::run();
+                println!("Part 1: {}", a);
+                println!("Part 2: {}", b);
+            }
+            2 => {
+                let (a, b) = aoc::day02::run();
+                println!("Part 1: {}", a);
+                println!("Part 2: {}", b);
+            }
+            3 => {
+                let (a, b) = aoc::day03::run();
+                println!("Part 1: {}", a);
+                println!("Part 2: {}", b);
+            }
+            4 => {
+                let (a, b) = aoc::day04::run();
+                println!("Part 1: {}", a);
+                println!("Part 2: {}", b);
+            }
+            5 => {
+                let (a, b) = aoc::day05::run();
+                println!("Part 1: {}", a);
+                println!("Part 2: {}", b);
+            }
+            6 => {
+                let (a, b) = aoc::day06::run();
+                println!("Part 1: {}", a);
+                println!("Part 2: {}", b);
+            }
+            7 => {
+                let (a, b) = aoc::day07::run();
+                println!("Part 1: {}", a);
+                println!("Part 2: {}", b);
+            }
+            _ => {
+                panic!("Unhandled day");
+            }
+        };
+    }
 }
