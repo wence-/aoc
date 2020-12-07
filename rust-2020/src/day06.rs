65548348ef32af7451c08a95fe4097c4bc56c7bf
Lawrence Mitchell <lawrence@wence.uk>
2020-12-07 22:58:55 +0000

    BitSets not hashtables

--- rust-2020/src/day06.rs
+++ rust-2020/src/day06.rs
@@ -1,40 +1,44 @@
-use std::collections;
 use std::fs;
 use std::path::PathBuf;
 
-fn read() -> Vec<(usize, collections::HashMap<char, usize>)> {
+fn read() -> (Vec<u32>, Vec<u32>) {
     let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     d.push("inputs/day06.input");
     let contents = fs::read_to_string(d).expect("ARGH, didn't read");
-    let mut vec = Vec::<(usize, collections::HashMap<char, usize>)>::new();
+    let mut veca = Vec::<u32>::new();
+    let mut vecb = Vec::<u32>::new();
     for group in contents.trim().split("\n\n") {
-        let mut data = collections::HashMap::<char, usize>::new();
-        let mut n: usize = 0;
-        for line in group.trim().split("\n") {
-            for c in line.chars() {
-                *data.entry(c).or_insert(0) += 1;
-            }
-            n += 1;
-        }
-        vec.push((n, data));
+        let p1 = group
+            .bytes()
+            .filter(|&c| c != b'\n')
+            .fold(0_u32, |acc, choice| acc | 1 << (choice - b'a'))
+            .count_ones();
+        let p2 = group
+            .split('\n')
+            .map(|person| {
+                person
+                    .bytes()
+                    .fold(0_u32, |acc, choice| acc | 1 << (choice - b'a'))
+            })
+            .fold(std::u32::MAX, |acc, one| acc & one)
+            .count_ones();
+        veca.push(p1);
+        vecb.push(p2);
     }
-    return vec;
+    return (veca, vecb);
 }
 
-fn part1(data: &Vec<(usize, collections::HashMap<char, usize>)>) -> usize {
-    return data.iter().map(|(_, g)| g.len()).sum();
+fn part1(data: &Vec<u32>) -> u32 {
+    return data.iter().sum::<u32>();
 }
 
-fn part2(data: &Vec<(usize, collections::HashMap<char, usize>)>) -> usize {
-    return data
-        .iter()
-        .map(|(n, g)| g.values().filter(|v| *v == n).count())
-        .sum();
+fn part2(data: &Vec<u32>) -> u32 {
+    return data.iter().sum::<u32>();
 }
 
-pub fn run() -> (usize, usize) {
-    let data = read();
-    let p1 = part1(&data);
-    let p2 = part2(&data);
+pub fn run() -> (u32, u32) {
+    let (d1, d2) = read();
+    let p1 = part1(&d1);
+    let p2 = part2(&d2);
     return (p1, p2);
 }
