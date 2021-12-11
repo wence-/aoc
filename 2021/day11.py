64e1db7df0ecc90b9da0916df10f754810423c7a
Lawrence Mitchell <lawrence@wence.uk>
2021-12-11 16:39:19 +0000

    Minor

--- 2021/day11.py
+++ 2021/day11.py
@@ -1,57 +1,66 @@
-from itertools import product
 from typing import Iterable
 
 T = tuple[int, int]
-OCTS = dict[tuple[int, int], int]
+To = dict[tuple[int, int], int]
 
 with open("../inputs/2021/day11.input", "r") as f:
     data = f.read()
     data = data.strip().split("\n")
-    octs: OCTS = {}
+    octopuses: To = {}
     for i, line in enumerate(data):
         for j, c in enumerate(map(int, line.strip())):
-            octs[i, j] = c
-    N, M = map(max, zip(*octs))
+            octopuses[i, j] = c
+    N, M = map(max, zip(*octopuses))
     N += 1
     M += 1
 
 
 def neighbours(i: int, j: int) -> Iterable[T]:
-    for di, dj in product((-1, 0, 1), (-1, 0, 1)):
-        if 0 <= i + di < N and 0 <= j + dj < M and not (di == 0 and dj == 0):
-            yield (i + di, j + dj)
+    for di, dj in (
+        (i - 1, j - 1),
+        (i, j - 1),
+        (i + 1, j - 1),
+        (i - 1, j),
+        (i + 1, j),
+        (i - 1, j + 1),
+        (i, j + 1),
+        (i + 1, j + 1),
+    ):
+        if 0 <= di < N and 0 <= dj < M:
+            yield di, dj
 
 
-def step(octs: OCTS) -> int:
-    for k in octs:
-        octs[k] += 1
-    seeds = set(k for k, v in octs.items() if v > 9)
-    flashed = set()
-    while seeds:
-        seed = seeds.pop()
-        flashed.add(seed)
-        for idx in neighbours(*seed):
-            octs[idx] += 1
-            if idx not in flashed and octs[idx] > 9:
-                seeds.add(idx)
-    for idx in flashed:
-        octs[idx] = 0
-    return len(flashed)
+def step(octopuses: To) -> int:
+    for k in octopuses:
+        octopuses[k] += 1
+    lifo = [k for k, v in octopuses.items() if v > 9]
+    seen = set()
+    while lifo:
+        seed = lifo.pop()
+        if seed not in seen:
+            seen.add(seed)
+            for i in neighbours(*seed):
+                octopuses[i] += 1
+                if i not in seen and octopuses[i] > 9:
+                    lifo.append(i)
+    for idx in seen:
+        octopuses[idx] = 0
+    return len(seen)
 
 
-def part1(octs: OCTS) -> int:
-    octs = octs.copy()
-    return sum(step(octs) for _ in range(100))
+def part1(octopuses: To) -> int:
+    octopuses = octopuses.copy()
+    return sum(step(octopuses) for _ in range(100))
 
 
-def part2(octs: OCTS) -> int:
-    octs = octs.copy()
+def part2(octopuses: To) -> int:
+    octopuses = octopuses.copy()
     i = 0
-    while any(v != 0 for v in octs.values()):
-        step(octs)
+    while any(v != 0 for v in octopuses.values()):
+        step(octopuses)
         i += 1
     return i
 
 
-print(part1(octs))
-print(part2(octs))
+print(part1(octopuses))
+print(part2(octopuses))
