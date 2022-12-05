1d3349f1c7d9b9899797a6b73d5f896a39a2bbee
Lawrence Mitchell <lawrence@wence.uk>
2022-12-05 00:13:47 +0000

    Minor

--- rust-2022/src/day01.rs
+++ rust-2022/src/day01.rs
@@ -1,6 +1,4 @@
-type I = u32;
-
-pub fn read(inp: &[u8]) -> Vec<I> {
+pub fn read(inp: &[u8]) -> Vec<u32> {
     let mut v = Vec::with_capacity(256);
     let mut val = 0;
     for line in inp.split(|&c| c == b'\n') {
@@ -16,12 +14,12 @@ pub fn read(inp: &[u8]) -> Vec<I> {
     v
 }
 
-pub fn part1(inp: &[I]) -> I {
-    inp.iter().fold(I::MIN, |acc, &i| acc.max(i))
+pub fn part1(inp: &[u32]) -> u32 {
+    inp.iter().fold(u32::MIN, |acc, &i| acc.max(i))
 }
 
 #[inline]
-fn sort(r: &mut [I]) {
+fn sort(r: &mut [u32]) {
     if r[0] > r[1] {
         r.swap(0, 1)
     }
@@ -33,7 +31,7 @@ fn sort(r: &mut [I]) {
     }
 }
 
-pub fn part2(inp: &[I]) -> I {
+pub fn part2(inp: &[u32]) -> u32 {
     const N: usize = 3;
     let mut r = [0; N];
     for &e in inp.iter() {
