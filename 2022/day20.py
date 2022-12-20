ce4eb1e316896edb7ca191cb1f4ce51439ca42e2
Lawrence Mitchell <lawrence@wence.uk>
2022-12-20 13:11:00 +0000

    Bucket list for day 20 (down to 180ms)

--- 2022/day20.py
+++ 2022/day20.py
@@ -1,60 +1,75 @@
 import time
-from collections import deque
+from typing import Generic, Iterable, TypeVar
 
 start = time.time()
+
+
 with open("../inputs/2022/day20.input") as f:
-    inp = list(enumerate(map(int, f.read().split("\n"))))
+    inp = list(map(int, f.read().split("\n")))
+
+T = TypeVar("T")
+
+
+class BucketList(Generic[T]):
+    def __init__(self, bucket_size, data: Iterable[T]):
+        self.bucket_size = bucket_size
+        self.buckets = [[]]
+        self.location = {}
+        for d in data:
+            self.add(d)
+
+    def add(self, v: T):
+        n = len(self.buckets) - 1
+        bucket = self.buckets[n]
+        if len(bucket) == self.bucket_size:
+            bucket = []
+            self.buckets.append(bucket)
+            n += 1
+        self.location[v] = n
+        self.buckets[n].append(v)
+
+    def shift(self, v: T, offset: int):
+        b = self.location[v]
+        offset += self.buckets[b].index(v)
+        self.buckets[b].remove(v)
+        while offset > (n := len(self.buckets[b])):
+            offset -= n
+            b = (b + 1) % len(self.buckets)
+        self.buckets[b].insert(offset, v)
+        self.location[v] = b
+
+    def index(self, v: T) -> int:
+        b = self.location[v]
+        pos = self.buckets[b].index(v)
+        pos += sum(len(self.buckets[i]) for i in range(b))
+        return pos
+
+    def __getitem__(self, i: int) -> T:
+        b = 0
+        while (i := i - len(self.buckets[b])) >= 0:
+            b += 1
+        return self.buckets[b][i]
+
+
+def solve(inp: list[int], multiplier: int, rounds: int):
+    zero = (inp.index(0), 0)
+    data = [(i, n * multiplier) for i, n in enumerate(inp)]
+    buckets = BucketList(128, data)
+    N = len(data)
+    for _ in range(rounds):
+        for d in data:
+            buckets.shift(d, d[1] % (N - 1))
+
+    zero_offset = buckets.index(zero)
+    return sum((buckets[(zero_offset + i * 1000) % N][1] for i in range(1, 4)))
 
 
 def part1(inp: list) -> int:
-    stuff = list(inp)
-    N = len(inp)
-
-    def loc(idx):
-        for j, (i, n) in enumerate(stuff):
-            if i == idx:
-                return (j, n)
-        raise ValueError
-
-    def pos(v):
-        for j, (_, n) in enumerate(stuff):
-            if n == v:
-                return (j, n)
-        raise ValueError
-
-    for i in range(len(inp)):
-        j, n = loc(i)
-        stuff.remove((i, n))
-        stuff.insert((j + n) % (N - 1), (i, n))
-
-    j, n = pos(0)
-    return sum(stuff[(j + n) % N][1] for n in [1000, 2000, 3000])
+    return solve(inp, 1, 1)
 
 
 def part2(inp: list) -> int:
-    stuff = list(((i, n * 811589153) for i, n in inp))
-    N = len(inp)
-
-    def loc(idx):
-        for j, (i, n) in enumerate(stuff):
-            if i == idx:
-                return (j, n)
-        raise ValueError
-
-    def pos(v):
-        for j, (_, n) in enumerate(stuff):
-            if n == v:
-                return (j, n)
-        raise ValueError
-
-    for _ in range(10):
-        for i in range(len(inp)):
-            j, n = loc(i)
-            stuff.remove((i, n))
-            stuff.insert((j + n) % (N - 1), (i, n))
-
-    j, n = pos(0)
-    return sum(stuff[(j + n) % N][1] for n in [1000, 2000, 3000])
+    return solve(inp, 811589153, 10)
 
 
 print(
